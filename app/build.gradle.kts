4de9624cfaaeea5600832546c16ffeb2eade806f
Yasahiru <legendem718@gmail.com>
2024-12-09 00:59:09 +0100

    setting room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
 }
